6b311c31c193d59a850695bf22648a3d73c0024c
ronaldlately-eng <ronaldlately@gmail.com>
2026-06-08 11:45:38 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PhoneSystem from './PhoneSystem';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><PhoneSystem /></React.StrictMode>);
